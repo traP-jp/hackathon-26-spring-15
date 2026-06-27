82b3d165709829d5514d355ba35950e13222989e
kageki128 <kagemakopf@gmail.com>
2026-06-27 23:05:32 +0900

    NearとFarのスプライトを適用

--- Assets/_MyProject/Scripts/View/Scene/Game/WorldSpawner.cs
+++ Assets/_MyProject/Scripts/View/Scene/Game/WorldSpawner.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
@@ -10,26 +9,19 @@ namespace MyProject.View
     public class WorldSpawner : ViewBase
     {
         [SerializeField]
-        Transform playerTransform;
-
-        [SerializeField]
-        GameObject floor;
-        [SerializeField]
-        Transform floorParent;
-
-        //左右に生成する床の数
-        uint floorMargin = 2;
-        List<GameObject> floors;
-        float floorWidth;
-
-        [SerializeField]
         GameObject nearBackground;
         [SerializeField]
         Transform nearBackgroundParent;
         [SerializeField]
+        float nearBackgroundWidth = 19.2f;
+        [SerializeField]
         GameObject farBackground;
         [SerializeField]
         Transform farBackgroundParent;
+        [SerializeField]
+        float farBackgroundWidth = 19.2f;
+        [SerializeField]
+        List<GameObject> sampleGameObjects = new();
 
         ParallaxBackground nearParallaxBackground;
         ParallaxBackground farParallaxBackground;
@@ -50,22 +42,18 @@ namespace MyProject.View
             /// <param name="camera">背景が追従するカメラ</param>
             /// <param name="background">背景のPrefab</param>
             /// <param name="parent">生成した背景を配置する親</param>
+            /// <param name="width">背景のループ幅</param>
             /// <param name="margin">左右にいくつ背景を複製するか</param>
             /// <param name="factor">視差の強さ。0に近づくほどカメラに追従して変化し、1に近づくほどスクロールが遅くなる</param>
             /// <param name="offsetY">Y方向の背景のオフセット</param>
-            public ParallaxBackground(Camera camera, GameObject background, Transform parent, uint margin, float factor, float offsetY)
+            public ParallaxBackground(Camera camera, GameObject background, Transform parent, float width, uint margin, float factor, float offsetY)
             {
-                SpriteRenderer spriteRenderer = background.GetComponent<SpriteRenderer>();
-
-                if (spriteRenderer == null)
+                if (width <= 0f)
                 {
-                    Debug.LogError("background does not have SpriteRenderer.");
-                }
-                else
-                {
-                    width = spriteRenderer.sprite.rect.width / spriteRenderer.sprite.pixelsPerUnit * background.transform.localScale.x;
+                    throw new InvalidOperationException("background width must be greater than 0.");
                 }
 
+                this.width = width;
                 this.camera = camera;
                 this.margin = margin;
                 this.factor = factor;
@@ -127,6 +115,7 @@ namespace MyProject.View
                 camera,
                 nearBackground,
                 nearBackgroundParent,
+                nearBackgroundWidth,
                 1,
                 0f,
                 0f
@@ -135,38 +124,13 @@ namespace MyProject.View
                 camera,
                 farBackground,
                 farBackgroundParent,
+                farBackgroundWidth,
                 1,
-                0.5f,
+                0.8f,
                 0f
             );
 
-            floors = new List<GameObject>();
-            for (int i = 0; i < 1 + floorMargin * 2; i++)
-            {
-                floors.Add(Instantiate(floor, floorParent));
-            }
-
-            BoxCollider2D collider = floor.GetComponent<BoxCollider2D>();
-
-            if(collider == null)
-            {
-                Debug.LogError("A BoxCollider is not attached.");
-            }
-            else
-            {
-                floorWidth = Vector2.Scale(collider.size, floor.transform.localScale).x;
-                int playerIndex = GetIndex(GetPlayerPosition().x);
-                for (int i = 0; i < floors.Count; i++)
-                {
-                    Transform transform = floors[i].transform;
-                    transform.localPosition = new Vector3(
-                        IndexToPosition(playerIndex - (int)floorMargin + i),
-                        transform.localPosition.y,
-                        transform.localPosition.z
-                    );
-                }
-            }
-
+            HideSampleGameObjects();
             gameObject.SetActive(false);
         }
 
@@ -194,69 +158,16 @@ namespace MyProject.View
 
         void Update()
         {
-            GenerateFloor();
             nearParallaxBackground.Update();
             farParallaxBackground.Update();
         }
 
-        void GenerateFloor()
+        void HideSampleGameObjects()
         {
-            int playerIndex = GetIndex(GetPlayerPosition().x);
-            int currentCenter = GetIndex(floors[(int)floorMargin].transform.localPosition.x);
-            //プレイヤーが属するIndexが変化したら床の位置を更新する
-            if(playerIndex < currentCenter)
+            foreach (GameObject sampleGameObject in sampleGameObjects)
             {
-                int offset = currentCenter - playerIndex;
-                int firstIndex = GetIndex(floors.First().transform.localPosition.x);
-                for(int i = 0; i < offset; i++)
-                {
-                    GameObject last = floors.Last();
-                    floors.RemoveAt(floors.Count - 1);
-
-                    last.transform.localPosition = new Vector3(
-                        IndexToPosition(firstIndex - i - 1),
-                        last.transform.localPosition.y,
-                        last.transform.localPosition.z
-                    );
-
-                    floors.Insert(0, last);
-                }
+                sampleGameObject.SetActive(false);
             }
-            else if(currentCenter < playerIndex)
-            {
-                int offset = playerIndex - currentCenter;
-                int lastIndex = GetIndex(floors.Last().transform.localPosition.x);
-                for(int i = 0; i < offset; i++)
-                {
-                    GameObject first = floors.First();
-                    floors.RemoveAt(0);
-
-                    first.transform.localPosition = new Vector3(
-                        IndexToPosition(lastIndex + i + 1),
-                        first.transform.localPosition.y,
-                        first.transform.localPosition.z
-                    );
-
-                    floors.Add(first);
-                }
-            }
-        }
-
-        Vector3 GetPlayerPosition()
-        {
-            return floorParent == null
-                ? playerTransform.position
-                : floorParent.InverseTransformPoint(playerTransform.position);
-        }
-
-        int GetIndex(float x)
-        {
-            return (int)MathF.Round(x / floorWidth);
-        }
-
-        float IndexToPosition(int index)
-        {
-            return index * floorWidth;
         }
     }
 }
